5aea6dd13a6213f13b96ba72b9e9fed98b3d4241
gideon <edoghotugiddy@gmail.com>
2022-10-14 01:11:58 +0100

    Implemented the Graph display using LinkedList

--- TreeDs/Program.cs
+++ TreeDs/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TreeDs;
+
 Console.WriteLine("Hello, World!");
+
+#region Type 1 : Graph Using LinkedList
+BasicGraph graph = new BasicGraph(8);
+graph.addEdge(0, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 4);
+graph.addEdge(2, 5);
+graph.addEdge(3, 5);
+graph.addEdge(4, 6);
+graph.addEdge(6, 7);
+graph.PrintAdjanceyList();
+graph.CreateAdjanceyMatrix(graph);
+#endregion
+
+
+
+
+
+
+
+Console.ReadLine();
